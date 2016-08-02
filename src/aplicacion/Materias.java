1d9546b96e71300b1cb473dde931ace13069ace8
Ana <Ana@Anacatalina>
2016-08-02 12:38:47 -0500

    avance final para presentacion

--- src/aplicacion/Materias.java
+++ src/aplicacion/Materias.java
@@ -152,19 +152,20 @@ public class Materias extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        Prinicipa pp1= new Prinicipa();
+        Prinicipa1 pp1= new Prinicipa1();
             pp1.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        Prinicipa pp1= new Prinicipa();
+        Prinicipa2 pp1= new Prinicipa2();
             pp1.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-        dispose();
+         Regui l= new Regui();
+        l.setVisible(true);
         
     }//GEN-LAST:event_jButton4ActionPerformed
 
